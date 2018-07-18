d580d6470a335b48e24c1cd928c96791a1c3fa9d
zEduardofaria <dudufaria95@gmail.com>
2018-07-18 20:54:08 -0300

    Playing with querys

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,10 +1,39 @@
-const MongoClient = require('mongodb').MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+var obj = new ObjectID()
+console.log('obj: ', obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   if (err) {
     console.log('Unable to connect to MongoDB service.')
-  } 
+  }
   console.log('Connected to MongoDB server')
+  const db = client.db('TodoApp')
 
-  db.close()
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err)
+  //   }
+
+  //   console.log(JSON.stringify(result.ops, undefined, 2))
+  // })
+
+  // Insert new doc into Users (name, age, location)
+
+  // db.collection('Users').insertOne({
+  //   name: 'Eduardo Faria Silva',
+  //   age: '22',
+  //   location: 'Betim MG'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err)
+  //   }
+
+  //   console.log(JSON.stringify(result.ops, undefined, 2))
+  // })
+
+  client.close()
 })
---
+++ playground/mongodb-find.js
@@ -0,0 +1,22 @@
+const { MongoClient, ObjectID } = require('mongodb')
+
+var obj = new ObjectID()
+console.log('obj: ', obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    console.log('Unable to connect to MongoDB service.')
+  }
+  console.log('Connected to MongoDB server')
+  const db = client.db('TodoApp')
+
+  db.collection('Todos').find({ completed: false }).toArray().then((docs) => {
+    console.log('Todos');
+    console.log(JSON.stringify(docs, undefined, 2));
+    
+  }, (err) => {
+    console.log('Unable to fetch Todos ', err)
+  })
+
+  client.close()
+})
