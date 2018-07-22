ec1c02592d06b2ae6b0cadca20e30c2e2cec0ef7
zEduardofaria <dudufaria95@gmail.com>
2018-07-22 12:14:57 -0300

    Add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-var express = require('express')
-var bodyParser = require('body-parser')
+const express = require('express')
+const bodyParser = require('body-parser')
+const { ObjectID } = require('mongodb')
 
-var { mongoose } = require('./db/mongoose')
-var { Todo } = require('./models/Todo')
-var { User } = require('./models/User')
+const { mongoose } = require('./db/mongoose')
+const { Todo } = require('./models/Todo')
+const { User } = require('./models/User')
 
 var app = express()
 
@@ -26,6 +27,23 @@ app.get('/todos', (req, res) => {
     res.send({ todos })
   }, (e) => {
     res.status(400).send(e)
+  })
+})
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      res.status(402).send()
+    }
+
+    res.send({ todo })
+  }).catch((e) => {
+    res.status(400).send()
   })
 })
 
