500f1db881f223c0ad575e533a01b4da1b4d4134
zEduardofaria <dudufaria95@gmail.com>
2018-07-22 12:28:55 -0300

    Test GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.get('/todos/:id', (req, res) => {
 
   Todo.findById(id).then((todo) => {
     if (!todo) {
-      res.status(402).send()
+      res.status(404).send()
     }
 
     res.send({ todo })
