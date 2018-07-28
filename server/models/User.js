9b7cd7369aa3e31f0d4fe529cc59744bda98246a
zEduardofaria <dudufaria95@gmail.com>
2018-07-27 22:33:15 -0300

    Add POST /users

--- server/models/User.js
+++ server/models/User.js
@@ -1,13 +1,34 @@
-var mongoose = require('mongoose')
+const mongoose = require('mongoose')
+const validator = require('validator')
 
 
 var User = mongoose.model('User', {
   email: {
     type: String,
     required: true,
+    trim: true,
     minLength: 1,
-    trim: true
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
   },
+  password: {
+    type: String,
+    require: true,
+    minLength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 })
 
 module.exports = { User }
